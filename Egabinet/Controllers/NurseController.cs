e20a161a8d8d3c7f428a4e617a6604d7b5602214
Honorata92 <h.sajdak1@o2.pl>
2022-12-27 22:36:01 +0100

    add_nurse_view

---
+++ Egabinet/Controllers/NurseController.cs
@@ -0,0 +1,74 @@
+﻿using Egabinet.Data;
+using Egabinet.Models;
+using Egabinet.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Egabinet.Controllers
+{
+    public class NurseController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public NurseController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            var user = dbContext.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+            return View(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditNurse()
+       
+        {
+            var employee = await dbContext.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+
+            if (employee != null)
+            {
+
+                var viewModel = new UpdateNurseViewModel()
+                {
+                    Id = employee.Id,
+                    Surname = employee.Surname,
+                    DateOfBirth = employee.DateOfBirth,
+                    Role = employee.Role,
+
+
+                };
+
+                return await Task.Run(() => View("EditNurse", viewModel));
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateNurseViewModel model)
+        {
+
+            var employee = await dbContext.Users.FindAsync(model.Id);
+            if (employee != null)
+            {
+        
+                employee.Surname = model.Surname;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Role = model.Role;
+
+
+                await dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+
+            }
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ Egabinet/Models/UpdateNurseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Egabinet.Models
+{
+    public class UpdateNurseViewModel
+    {
+
+            public string Id { get; set; }
+            public string Surname { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string Role { get; set; }
+
+        
+    }
+
+
+}
+

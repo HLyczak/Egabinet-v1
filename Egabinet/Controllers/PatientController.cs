6ee93503ecc975d6961ced196c1e99b5ecbc3b4a
Honorata92 <h.sajdak1@o2.pl>
2023-01-22 15:23:43 +0100

    add patient view

--- Egabinet/Controllers/PatientController.cs
+++ Egabinet/Controllers/PatientController.cs
@@ -1,6 +1,8 @@
 ﻿using Egabinet.Data;
+using Egabinet.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Egabinet.Controllers
 {
@@ -19,5 +21,37 @@ namespace Egabinet.Controllers
             Microsoft.AspNetCore.Identity.IdentityUser? user = _dbContext.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
             return View(user);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ShowTimesheet()
+        {
+            var viewModel = await _dbContext.TimeSheet.Select(t => new TimeSheetViewModel { Patient = t.Patient.Name, Doctor = t.Doctor.Name, Room = t.Room.Number, Date = t.Data, Id = t.Id }).ToListAsync();
+
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteVisit(string id)
+        {
+            try
+            {
+                var visitToDelete = await _dbContext.TimeSheet.FindAsync(id);
+                if (visitToDelete != null)
+                {
+                    _dbContext.TimeSheet.Remove(visitToDelete);
+                    await _dbContext.SaveChangesAsync();
+                }
+
+                return visitToDelete == null ? NotFound($"Visit with Id = {id} not found") : RedirectToAction("Index");
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting data");
+            }
+
+        }
     }
+
 }

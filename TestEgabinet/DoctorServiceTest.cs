4e92d958fbd95f6f71477ea8e85a56658d4f7f2b
Honorata92 <h.sajdak1@o2.pl>
2023-02-17 22:20:33 +0100

    fix yaml

--- TestEgabinet/DoctorServiceTest.cs
+++ TestEgabinet/DoctorServiceTest.cs
@@ -13,16 +13,16 @@ namespace TestEgabinet
         public async Task GetDoctorAsync_Invoke()
         {
             //Arrange
-            var timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var doctorRepositoryMock = new Mock<IDoctorRepository>();
+            Mock<ITimesheetRepository> timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
+            Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
+            Mock<IDoctorRepository> doctorRepositoryMock = new Mock<IDoctorRepository>();
 
             doctorRepositoryMock.Setup(d => d.GetByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(new Doctor() { Id = "1", Name = "Janek" }));
 
-            var doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
+            DoctorService doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
 
             // Act
-            var resultUser = await doctorService.GetDoctorAsync("janek@gmail.com");
+            DoctorViewModel resultUser = await doctorService.GetDoctorAsync("janek@gmail.com");
 
             // Assert
             Assert.Equal("1", resultUser.Id);
@@ -33,16 +33,16 @@ namespace TestEgabinet
         public async Task GetUpdateDoctorViewModel_Invoke()
         {
             //Arrange
-            var timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var doctorRepositoryMock = new Mock<IDoctorRepository>();
+            Mock<ITimesheetRepository> timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
+            Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
+            Mock<IDoctorRepository> doctorRepositoryMock = new Mock<IDoctorRepository>();
 
             doctorRepositoryMock.Setup(d => d.GetByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(new Doctor() { Id = "1", Name = "Janek" }));
 
-            var doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
+            DoctorService doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
 
-            // Act
-            var resultUser = await doctorService.GetUpdateDoctorViewModel("janek@gmail.com");
+            // Act execute
+            UpdateDoctorViewModel resultUser = await doctorService.GetUpdateDoctorViewModel("janek@gmail.com");
 
             // Assert
             Assert.Equal("1", resultUser.Id);
@@ -55,16 +55,16 @@ namespace TestEgabinet
         public async Task GetUser_Invoke()
         {
             //Arrange
-            var timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
-            var doctorRepositoryMock = new Mock<IDoctorRepository>();
-            var userRepositoryMock = new Mock<IUserRepository>();
+            Mock<ITimesheetRepository> timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
+            Mock<IDoctorRepository> doctorRepositoryMock = new Mock<IDoctorRepository>();
+            Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
 
             userRepositoryMock.Setup(d => d.GetByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(new IdentityUser() { Id = "1", Email = "janek@gmail.com" }));
 
-            var doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
+            DoctorService doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
 
             // Act
-            var resultUser = await doctorService.GetUser("janek@gmail.com");
+            IdentityUser resultUser = await doctorService.GetUser("janek@gmail.com");
 
             // Assert
             Assert.Equal("1", resultUser.Id);
@@ -77,15 +77,15 @@ namespace TestEgabinet
         public async Task UpdateDoctorAsync_Invoke()
         {
             //Arrange
-            var doctor = new Doctor() { Id = "1", Name = "Janek", Surname = "Nowak" };
-            var timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
-            var doctorRepositoryMock = new Mock<IDoctorRepository>();
-            var userRepositoryMock = new Mock<IUserRepository>();
+            Doctor doctor = new Doctor() { Id = "1", Name = "Janek", Surname = "Nowak" };
+            Mock<ITimesheetRepository> timeSheetRepositoryMock = new Mock<ITimesheetRepository>();
+            Mock<IDoctorRepository> doctorRepositoryMock = new Mock<IDoctorRepository>();
+            Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
 
             doctorRepositoryMock.Setup(d => d.GetByIdAsync(It.IsAny<string>())).Returns(Task.FromResult(doctor));
             doctorRepositoryMock.Setup(d => d.UpdateAsync(It.IsAny<Doctor>()));
 
-            var doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
+            DoctorService doctorService = new DoctorService(userRepositoryMock.Object, timeSheetRepositoryMock.Object, doctorRepositoryMock.Object);
 
             // Act
             await doctorService.UpdateDoctorAsync(new UpdateDoctorViewModel() { Name = "Adam", Surname = "Kowalski" });
